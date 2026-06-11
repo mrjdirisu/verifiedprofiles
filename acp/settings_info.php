aed7b7d83e19a39dc0150f2b344198d644c32adf
Irisu june <mrjbrickman@gmail.com>
2026-06-11 07:02:29 +0100

    Update namespace and copyright information
    Facebook

--- acp/settings_info.php
+++ acp/settings_info.php
@@ -2,23 +2,23 @@
 
 /**
  * @package Verified Profiles
- * @copyright (c) 2026 Daniel James
+ * @copyright (c) 2026 Ötäll Last King Xipappa
  * @license https://opensource.org/license/gpl-2-0
  */
 
-namespace danieltj\verifiedprofiles\acp;
+namespace otalllastking.xipappa\verifiedprofiles\acp;
 
 class settings_info {
 
 	public function module() {
 
 		return [
-			'filename'	=> '\danieltj\verifiedprofiles\acp\settings_module',
+			'filename'	=> '\otalllastking.xipappa\verifiedprofiles\acp\settings_module',
 			'title'		=> 'ACP_VERIFIED_PROFILES',
 			'modes'		=> [
 				'settings'	=> [
 					'title'	=> 'ACP_VERIFIED_PROFILES',
-					'auth'	=> 'ext_danieltj/verifiedprofiles && acl_a_board',
+					'auth'	=> 'ext_otalllastking.xipappa/verifiedprofiles && acl_a_board',
 					'cat'	=> [ 'ACP_CAT_USERS' ],
 				],
 			],
